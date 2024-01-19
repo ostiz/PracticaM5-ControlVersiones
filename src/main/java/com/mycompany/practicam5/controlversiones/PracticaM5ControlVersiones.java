ef8be4d3d415a5de249c651eea3fab26de772402
isard <isard@ubuntu>
2024-01-19 12:31:15 +0100

    otro cambio

--- src/main/java/com/mycompany/practicam5/controlversiones/PracticaM5ControlVersiones.java
+++ src/main/java/com/mycompany/practicam5/controlversiones/PracticaM5ControlVersiones.java
@@ -12,5 +12,6 @@ public class PracticaM5ControlVersiones {
 
     public static void main(String[] args) {
         System.out.println("Hello World! 788");
+                System.out.println("Hello World! 778");
     }
 }
