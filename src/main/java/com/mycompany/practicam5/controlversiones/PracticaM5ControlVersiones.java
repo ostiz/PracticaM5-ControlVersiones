024986f20ee2a46aeda6f8a57577a089fb9500fa
isard <isard@ubuntu>
2024-01-19 12:04:25 +0100

    inicio linux

---
+++ src/main/java/com/mycompany/practicam5/controlversiones/PracticaM5ControlVersiones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicam5.controlversiones;
+
+/**
+ *
+ * @author isard
+ */
+public class PracticaM5ControlVersiones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
